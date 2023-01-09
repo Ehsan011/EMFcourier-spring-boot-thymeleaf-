a5d75a829739c10c7564b0c9e6c951edbc4fcd23
Ehsan011 <ehsanhabibf@gmail.com>
2023-01-09 12:25:45 +0600

    New Project

---
+++ EMFCourier/src/main/java/org/idb/emf/EMFCourier/EmfCourierApplication.java
@@ -0,0 +1,13 @@
+package org.idb.emf.EMFCourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmfCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmfCourierApplication.class, args);
+	}
+
+}
