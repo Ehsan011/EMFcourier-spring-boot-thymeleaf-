53c5772992d03bd9b4d5c01af7a9a11a716ba8ba
jannatul_mawa <121705278+mawa333@users.noreply.github.com>
2023-01-09 13:26:48 +0600

    entityAdd

--- EMFCourier/src/main/java/org/idb/emf/EMFCourier/entity/Employee.java
+++ EMFCourier/src/main/java/org/idb/emf/EMFCourier/entity/Employee.java
@@ -1,13 +1,13 @@
 package org.idb.emf.EMFCourier.entity;
 
-<<<<<<< HEAD
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.naming.Name;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Data
@@ -15,15 +15,28 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Table(name="employee_data")
 public class Employee {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(length = 50, nullable = false)
     private String empFirstName;
+    @Column(length = 50, nullable = false)
     private String empLastName;
+    @Column(length = 50, nullable = false)
     private String empEmail;
+    @Column(length = 50, nullable = false)
     private String empContact;
+    @Column(length = 50, nullable = false)
     private String empAddress;
+    @Column(length = 50, nullable = false)
     private String empGender;
-    private String empDOB;
+    private int empCode;
+    @Column(length = 50, nullable = false)
+    private Date empDOB;//pk
+    private String empDepartment;//pk
+
+
+
 
 
 }
