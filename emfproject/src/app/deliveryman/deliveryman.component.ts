fa03b7482d3f9245f27161d8009d122cd6fe8e72
Ehsan011 <ehsanhabibf@gmail.com>
2023-01-29 10:41:12 +0600

    Add New UI Angular

---
+++ emfproject/src/app/deliveryman/deliveryman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deliveryman',
+  templateUrl: './deliveryman.component.html',
+  styleUrls: ['./deliveryman.component.css']
+})
+export class DeliverymanComponent {
+
+}
