e6c7898776da61b1ec9747efa942226fb198a8df
Farzana Juthi <w3farzana@gmail.com>
2023-02-01 11:17:23 +0600

    vbgh

--- EMFCourierService/src/main/java/com/example/EMFCourierService/repository/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EMFCourierService.repository;
-
-import com.example.EMFCourierService.entity.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<Driver, Integer> {
-}
